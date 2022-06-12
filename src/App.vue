a881341011c5ccc5e4299e773f3cc695c5ceb732
kongxiangyan <kcigaret@outlook.com>
2022-06-12 08:02:42 +0800

    refactor: lint template code

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,26 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <image
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">
+          Home
+        </RouterLink>
+        <RouterLink
+          to="/about"
+        >
+          About
+        </RouterLink>
       </nav>
     </div>
   </header>
