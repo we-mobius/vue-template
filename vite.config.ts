f1b3761010106d2235f77099d81ab488e79634c6
kongxiangyan <kcigaret@outlook.com>
2022-08-21 16:55:33 +0800

    feat: make tsconfig fully under control

--- vite.config.ts
+++ vite.config.ts
@@ -17,12 +17,12 @@ export default defineConfig({
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./', import.meta.url)) },
       { find: 'Src', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: 'Elements', replacement: fileURLToPath(new URL('./src/elements', import.meta.url)) },
-      { find: 'Components', replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
-      { find: 'Views', replacement: fileURLToPath(new URL('./src/views', import.meta.url)) },
       { find: 'Statics', replacement: fileURLToPath(new URL('./src/statics', import.meta.url)) },
       { find: 'Styles', replacement: fileURLToPath(new URL('./src/statics/styles', import.meta.url)) },
       { find: 'Images', replacement: fileURLToPath(new URL('./src/statics/images', import.meta.url)) },
+      { find: 'Elements', replacement: fileURLToPath(new URL('./src/elements', import.meta.url)) },
+      { find: 'Components', replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
+      { find: 'Views', replacement: fileURLToPath(new URL('./src/views', import.meta.url)) },
       { find: 'Services', replacement: fileURLToPath(new URL('./src/services', import.meta.url)) }
     ]
   },
